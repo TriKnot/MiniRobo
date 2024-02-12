a1a4e9caf8d8e28e215cae43c4b3481be4a15c13
TriKnot <90341216+TriKnot@users.noreply.github.com>
2024-02-12 11:34:53 +0100

    Create SmoothDynamicsIntegrator for reusable Vector Smoothing

---
+++ Source/MiniBot/Private/SmoothDynamicsIntegrator.cpp
@@ -0,0 +1,41 @@
+﻿#include "SmoothDynamicsIntegrator.h"
+#include "Math/UnrealMathUtility.h"
+
+USmoothDynamicsIntegrator::USmoothDynamicsIntegrator()
+	:	K1(0), K2(0), K3(0),
+		ResponseFrequency(1.0f), 
+		ResponseDamping(1.0f),
+		ResponseUnderShoot(0.0f),
+		X0(FVector::ZeroVector)
+{
+}
+
+void USmoothDynamicsIntegrator::Initialize(const FVector& InitialPosition, float Frequency, float Damping, float UnderShoot)
+{
+	X0 = InitialPosition;
+	ResponseFrequency = Frequency;
+	ResponseDamping = Damping;
+	ResponseUnderShoot = UnderShoot;
+
+	K1 = ResponseDamping / (PI * ResponseFrequency);
+	K2 = 1 / ((2 * PI * ResponseFrequency) * (2 * PI * ResponseFrequency));
+	K3 = ResponseUnderShoot * ResponseDamping / (2 * PI * ResponseFrequency);
+
+	Previous = X0;
+	Current = X0;
+	Delta = FVector::ZeroVector;
+}
+
+FVector USmoothDynamicsIntegrator::Update(float DeltaTime, const FVector& TargetPosition, FVector Velocity)
+{
+	if (Velocity.IsZero())
+	{
+		Velocity = (TargetPosition - Previous) / DeltaTime;
+	}
+	Previous = TargetPosition;
+
+	const float StableK2 = FMath::Max(K2, FMath::Max(DeltaTime * DeltaTime / 2 + DeltaTime * K1 / 2, DeltaTime * K1));
+	Current = Current + DeltaTime * Delta;
+	Delta = Delta + DeltaTime * (TargetPosition + K3 * Delta - Current - K1 * Delta) / StableK2;
+	return Current;
+}
---
+++ Source/MiniBot/Public/SmoothDynamicsIntegrator.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SmoothDynamicsIntegrator.generated.h"
+
+UCLASS()
+class MINIBOT_API USmoothDynamicsIntegrator : public UObject
+{
+	GENERATED_BODY()
+public:
+	USmoothDynamicsIntegrator();
+	void Initialize(const FVector& InitialPosition, float Frequency, float Damping, float UnderShoot);
+	FVector Update(float DeltaTime, const FVector& TargetPosition, FVector Velocity = FVector::ZeroVector);
+
+private:
+	FVector Previous;
+	FVector Current;
+	FVector Delta;
+	float K1, K2, K3;
+
+	// Parameters
+	float ResponseFrequency;
+	float ResponseDamping;
+	float ResponseUnderShoot;
+	FVector X0;
+};
